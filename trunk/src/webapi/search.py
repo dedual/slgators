338cdf177b7136d80ed2faec171771595d1a32f4
ghais.issa <ghais.issa@bcb3669e-2345-0410-bd11-09b7f980ec4f>
2008-03-03 03:20:19 +0000

    Converted amazon into a library by adding __init__.py Created a modulo to search amazon and compar against the database (Needs some work) Created a web api to search both amazon and PG at the same time

---
+++ trunk/src/webapi/search.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
+import MySQLdb
+from amazon import amazon_search
+
+def print_cont():
+    print "Content-type: text/html"
+    print
+
+def print_title():
+    print """
+    <html>
+
+    <head><title>SecondLife DB</title></head>
+
+    <body>
+
+    """
+    
+def handle_form():
+    form = cgi.FieldStorage()
+    title = form.getvalue("title")
+    page = form.getvalue('page')
+    result = amazon_search.group_searches(title, page)
+    if(result):
+        for title, id in result:
+            if id:
+                print "<p>" + title + "|" + str(id[0])
+                
+            else:
+                print "<p>" + title + '|None'
+    else:
+        print("Error")
+
+        
+        
+print_cont()
+print_title()
+handle_form()
