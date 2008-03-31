a06d784595e206756ae74cdf2e6ec612ce1e70b4
ghais.issa <ghais.issa@bcb3669e-2345-0410-bd11-09b7f980ec4f>
2008-03-31 18:47:39 +0000

    Modified bookmark.py so that it reflects the new database structre Faster execution and more robust

--- trunk/src/webapi/bookmark.py
+++ trunk/src/webapi/bookmark.py
@@ -2,7 +2,7 @@
 import cgi
 import cgitb; cgitb.enable()  # for troubleshooting
 import MySQLdb
-
+from user_db import user_info
 def print_cont():
     print "Content-type: text/html"
     print
@@ -18,62 +18,7 @@ def print_title():
     """
 
     
-def connect_to_database(databasename, usr, password):
-    db = MySQLdb.connect(host="localhost", user=usr, passwd=password, db=databasename)
-    return db
-
-def insertBookmark(fname, lname, bookmarkname, bookid, page):
-    db = connect_to_database("amazon", "root", "gitkotwg0")
-    cursor = db.cursor()
-    assert(isinstance(cursor, MySQLdb.cursors.Cursor))
-    cursor.execute("""SELECT `bookmark`.`id`
-    FROM bookmark, user
-    WHERE (
-    (
-    `bookmark`.`id` = user.bookmark_id
-    )
-    AND (
-    `user`.`fname` = '""" + fname + """'
-    )
-    AND (
-    `user`.`lname` = '""" + lname +"""'
-    )
-    AND (
-    `bookmark`.`page` = """ + page + """
-    )
-    ); """)
-
-    result = cursor.fetchone()
-    if not result:        
-        cursor.execute("""INSERT INTO `amazon`.`bookmark` (
-        `id` ,
-        `name` ,
-        `bookid` ,
-        `page` ,
-        `misc`
-        )
-        VALUES (
-        NULL ,'""" + MySQLdb.escape_string(bookmarkname) + "','" + bookid +"', '" + page +"""', NULL
-        );""")
-        cursor.execute("""SELECT MAX(`id`)
-        FROM `bookmark`
-        WHERE bookid = '""" + MySQLdb.escape_string(bookid) +"""'
-        AND page =""" + str(page) +";")
-        id = str(cursor.fetchone()[0])
-        cursor.execute("""INSERT INTO `amazon`.`user` (
-        `fname` ,
-        `lname` ,
-        `bookmark_id` ,
-        `languag`
-        )
-        VALUES (
-        '""" + fname +"""', '""" + lname + """', '""" + id + """', NULL
-        );""")
-        print 1
-    else:
-        print 0
-        
-        
+  
 def handle_form():
     form = cgi.FieldStorage()
     fname = form.getvalue("fname")
@@ -81,7 +26,7 @@ def handle_form():
     bookid = form.getvalue("bookid")
     page = form.getvalue("page")
     name = form.getvalue("name")
-    result = insertBookmark(fname, lname, name, bookid, str(page))
+    result = user_info.add_bookmark(fname, lname, name, bookid, str(page))
         
         
 
