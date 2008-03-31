bf083d40027441e56fe46910a5f6828a96f20194
ghais.issa <ghais.issa@bcb3669e-2345-0410-bd11-09b7f980ec4f>
2008-03-31 19:05:27 +0000

    Modified allbookmarks.py to reflect the new changes in database and bookmark handleing

--- src/user_db/user_info.py
+++ src/user_db/user_info.py
@@ -80,5 +80,27 @@ def add_bookmark(fname, lname,  bookmarkname, bookid, page):
         print 1
     except MySQLError:
         print 0
+
+
+def fetch_all_bookmarks(fname, lname, page = -1):
+    if page == -1:
+        return fetch_user_bookmarks(fname, lname)
+    else:
+        start = (page - 1) * 25
+        end = page * 25
+        db = connect_to_database("amazon", "root", "gitkotwg0")
+        cursor = db.cursor()
     
+        sql_query = ("""SELECT DISTINCT bookmark.id, bookmark.name, book.title, book.UUID, bookmark.page
+        FROM book, bookmark
+        WHERE bookmark.fname = '""" + fname + """'
+        AND bookmark.lname = '"""   + lname + """'
+        AND bookmark.bookid = book.id LIMIT """ + str(start) + """,""" + str(end) + """ ;""")
+        
+        cursor.execute(sql_query)
+        result = cursor.fetchall()
+        db.close()
+        return result
+
+
     
--- src/webapi/allbookmarks.py
+++ src/webapi/allbookmarks.py
@@ -2,6 +2,7 @@
 import cgi
 import cgitb; cgitb.enable()  # for troubleshooting
 import MySQLdb
+from user_db import user_info
 
 def print_cont():
     print "Content-type: text/html"
@@ -17,49 +18,42 @@ def print_title():
 
     """
 
-    
-def connect_to_database(databasename, usr, password):
-    db = MySQLdb.connect(host="localhost", user=usr, passwd=password, db=databasename)
-    return db
-
-
-
-def getAll(fname, lname, start='0', end='10'):
-    db = connect_to_database("amazon", "root", "gitkotwg0")
-    cursor = db.cursor()
-    assert(isinstance(cursor, MySQLdb.cursors.Cursor))
-    cursor.execute(""" SELECT DISTINCT `bookmark`.`name`, `bookmark`.`id`, `book`.`title`, `book`.`UUID`, `bookmark`.`page` FROM bookmark, book, user WHERE ((`bookmark`.`id` =user.bookmark_id) AND (`user`.`fname` ='""" + MySQLdb.escape_string(fname) + """') AND (`user`.`lname` ='""" + MySQLdb.escape_string(lname) +"""') AND (`book`.`id` =bookmark.bookid)) ORDER BY `book`.`title` ASC LIMIT """ + start +"," + end + """;""")
-    return cursor.fetchall()
-
-
 def handle_form():
     form = cgi.FieldStorage()
     fname = form.getvalue("fname")
     lname = form.getvalue("lname")
-    start = form.getvalue("start")
-    end = form.getvalue("end")
-    if start and end:
+    page = int(form.getvalue("page")) 
+    if page:
         result = ""
-        for row in getAll(fname, lname, start, end):
+        for row in user_info.fetch_all_bookmarks(fname, lname, page):
+            str_row = [str(x) for x in row]
+            print "|".join(str_row)
+            """
             for item in row:
                 result += str(item) + ","
             result.strip()
             result = result.replace(",", "|")
-            print result[:-1]       
+            print result
+            """
+            print "<P>"      
     else:
-        for row in getAll("Ghais", "Ireton"):
+        for row in user_info.fetch_all_bookmarks("Ghais", "Ireton"):
+            str_row = [str(x) for x in row]
+            print "|".join(str_row)
+            """
             result = ""
             for item in row:
                 result += str(item) + ","
             result.strip()
             result = result.replace(",", "|")
-            print result[:-1]
-            
+            print result
+            """
+            print "<P>"
 
         
 
 
 print_cont()
-print_title()
+#print_title()
 handle_form()
 
