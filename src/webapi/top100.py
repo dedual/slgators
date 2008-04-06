aeb8ed0a9a8b061697b3319295e9bdaec482adf3
ghais.issa <ghais.issa@bcb3669e-2345-0410-bd11-09b7f980ec4f>
2008-04-06 23:49:28 +0000

    Top 100 books has been implemented

---
+++ src/webapi/top100.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
+import MySQLdb
+from pg import top100
+from amazon import amazon_search
+
+
+
+
+def print_cont():
+    print "Content-type: text/html"
+    print
+
+def print_title():
+    print """
+    <html>
+
+    <head><title>SecondLife DB</title></head>
+
+    <body>
+
+    """
+    
+def handle_form():
+    form = cgi.FieldStorage()
+    page = 0
+    try:
+        page = int(form.getvalue('page'))
+    except TypeError, e:
+        print e
+    books = top100.get_top_100_books(int(page))
+    output = ""
+    for book_id in  books.keys():
+        asin = amazon_search.get_ASIN(book_id)
+        
+        print books[book_id]['title'] + "|" + book_id + "|" +  asin + "<p>"
+        
+print_cont()
+handle_form()
