98417cac14b013faf754ba26d704e45a58991c7d
ghais.issa <ghais.issa@bcb3669e-2345-0410-bd11-09b7f980ec4f>
2008-04-19 15:16:56 +0000

    Corrected an error in review where we don't set the amazon key correctly

--- trunk/src/webapi/review.py
+++ trunk/src/webapi/review.py
@@ -56,6 +56,7 @@ def get_ASIN(etextid):
                 print "KEYERROR" 
         else:
             try:
+                ecs.setLicenseKey('0ZW74MMABE2VX9H26182')
                 books = ecs.ItemSearch(title,SearchIndex='Books', Sort='relevancerank')
                 return books[0].ASIN
             except KeyError:
